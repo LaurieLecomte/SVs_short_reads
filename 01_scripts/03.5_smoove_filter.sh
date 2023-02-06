7b99773e0e86ea221bf19e674337d86cb67e8caf
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2023-02-06 16:27:45 -0500

    added 03.5_smoove_filter

---
+++ 01_scripts/03.5_smoove_filter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Filter for SV calls tagged as PRECISE in smoove's calls
+
+# srun -c 1 -p ibis_small -J 03.5_smoove_filter -o log/03.5_smoove_filter_%j.log /bin/sh 01_scripts/03.5_smoove_filter.sh &
+
+# VARIABLES
+GENOME="03_genome/genome.fasta"
+CHR_LIST="02_infos/chr_list.txt"
+BAM_DIR="04_bam"
+CALLS_DIR="05_calls"
+MERGED_DIR="06_merged"
+FILT_DIR="07_filtered"
+
+
+# LOAD REQUIRED MODULES
+module load bcftools/1.15
+
+# 1. Remove imprecise calls and BNDs
+bcftools filter -i 'IMPRECISE=0 & SVTYPE!="BND"' $MERGED_DIR/smoove/smoove_merged_sorted.vcf.gz -Oz > $FILT_DIR/smoove/smoove_PRECISE.vcf.gz
+tabix -p vcf $FILT_DIR/smoove/smoove_PRECISE.vcf.gz
