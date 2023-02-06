7b206d197bad09c73079692894843cdcd9bb5038
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2023-02-06 16:25:17 -0500

    added bcftools sort step

--- 01_scripts/01.4_delly_merge_samples.sh
+++ 01_scripts/01.4_delly_merge_samples.sh
@@ -4,7 +4,7 @@
 # SV calling is done by sample for high-coverage genomes or in small batches for low-coverage genomes : we have high coverage (16X)
 # Following instructions for germline SV calling (https://github.com/dellytools/delly#germline-sv-calling)
 
-# srun -c 1 -p ibis_small -J 01.4_delly_merge_samples -o log/01.4_delly_merge_samples_%j.log /bin/sh 01_scripts/01.4_delly_merge_samples.sh &
+# srun -c 1 -p ibis_small -J 01.4_delly_merge_samples -o log/01.4_delly_merge_samples_%j.log 01_scripts/01.4_delly_merge_samples.sh &
 
 # VARIABLES
 GENOME="03_genome/genome.fasta"
@@ -20,7 +20,7 @@ BCF_GENO_LIST="02_infos/delly_bcf_list.txt"
 
 # LOAD REQUIRED MODULES
 module load delly/1.1.6
-module load bcftools/1.15
+module load bcftools
 
 
 # 1. Make a list of bcf files to merge
@@ -35,5 +35,8 @@ ls -1 $CALLS_DIR/delly/geno/*.bcf > $BCF_GENO_LIST
 # 2. Merge all genotyped samples to get a single VCF/BCF using bcftools merge
 bcftools merge -m id -O b -o $MERGED_DIR/delly/merged_samples.bcf -l $BCF_GENO_LIST
 
-# 3. Convert bcf to vcf using bcftools, add tags and sort
-bcftools view $MERGED_DIR/delly/merged_samples.bcf | bcftools +fill-tags | bcftools sort -Oz > $MERGED_DIR/delly/delly_merged_sorted.vcf.gz
+# 3. Convert bcf to vcf using bcftools, add tags, sort and remove excluded contigs from header
+bcftools view $MERGED_DIR/delly/merged_samples.bcf | grep -vFf $REGIONS_EX | bcftools +fill-tags | bcftools sort -Oz > $MERGED_DIR/delly/delly_merged_sorted.vcf.gz
+
+# Clean up
+rm $MERGED_DIR/delly/merged_samples.bcf
