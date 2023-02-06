71c4a311a700e454d6f18efe35b3f2e6372edde2
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2023-02-06 16:29:26 -0500

    added formatting scripts for callers output

--- 01_scripts/01.6_delly_format.sh
+++ 01_scripts/01.6_delly_format.sh
@@ -13,12 +13,39 @@ MERGED_DIR="06_merged"
 FILT_DIR="07_filtered"
 
 
+REGIONS_EX="02_infos/excl_chrs.txt"
+
+DELLY_VCF="$FILT_DIR/delly/delly_PASS_PRECISE.vcf.gz"
+
+# LOAD REQUIRED MODULES
+module load bcftools
+module load r/4.1.2
 
 
-# 1. Add explicit ALT sequence for INVs, DELs
+# 1. Construct simplified header
+## Extract lines for fields other than INFO, FORMAT and bcftools commands
+bcftools view -h $DELLY_VCF | grep -E -v 'INFO|FORMAT|contig|bcftools|cmd' > $FILT_DIR/delly/VCF_lines.txt
+## Contigs lines, excluding regions/contigs in $REGIONS_EX
+bcftools view -h $DELLY_VCF | grep 'contig' | grep -vFf $REGIONS_EX > $FILT_DIR/delly/VCF_chrs.txt
 
+## Add line describing ALT seq INFO field
+echo '##INFO=<ID=ALT_DELLY,Number=1,Type=String,Description="Alternate sequence for SV">' >> $FILT_DIR/delly/VCF_lines.txt
 
-# 2. Add SVLEN field
+## Cat these together
+cat $FILT_DIR/delly/VCF_lines.txt 02_infos/shared_header_lines.txt $FILT_DIR/delly/VCF_chrs.txt > $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)".header
 
+# 2. Format VCF contents 
+Rscript 01_scripts/format_delly.R $DELLY_VCF $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)".contents $GENOME
 
-# 3. Simplify VCF
+# 3. Concatenate header with contents
+cat $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)".header $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)".contents | bcftools sort > $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)"_formatted.vcf
+
+# Index 
+#bgzip $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)"_formatted.vcf
+#tabix -p vcf $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)"_formatted.vcf.gz
+
+# Clean up 
+rm $FILT_DIR/delly/VCF_lines.txt
+rm $FILT_DIR/delly/VCF_chrs.txt
+rm $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)".header
+rm $FILT_DIR/delly/"$(basename -s .vcf.gz $DELLY_VCF)".contents
